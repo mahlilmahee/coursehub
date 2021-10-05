90224f570b5d4acc6cfe2bce300287cb426535e7
acc username <acc email>
2021-10-05 18:45:37 +0600

    almost

--- my-app/src/Component/Review/Contact.js
+++ my-app/src/Component/Review/Contact.js
@@ -1,20 +1,31 @@
 import React from 'react';
-
+import img from '../../images/unnamed.jpg'
 const Contact = () => {
     const handlingEmail=()=>{
       const Submit =document.getElementById("ami");
               Submit.value='We will soon contact with you  ';
     }
+    // const  style={  backgroundImage: `url(${img})`,noRepeat:"none"}; 
     return (
-        <div className="border-2 border-green-300 m-10 rounded-lg">
+        <div>
+           <div className='grid grid-cols-2 gap-4 items-center'> 
+            <img src={img} /> 
+            <div> 
+
+                <h3 className="w-3/4"> We are always here only for your help , if your have any query just mail us or inform us about your problem .We will contact with you as soon as possible from us . Thank you very much for being with us .</h3>
+            </div>
+
+            </div>
+            <div className="border-2 border-green-300 m-10 rounded-lg">
                 <h3 className="text-4xl mb-20 mt-10"> Contact us here  </h3> 
-             <span> Question:</span>  
+             <span> Here : </span>  
               <input id="ami" type="text" className="w-1/2 border-2 border-red-500 rounded-lg mb-20" placeholder="please insert your query here " />
-               <button onClick={handlingEmail} className='bg-green-500 p-2 rounded-lg' >Send</button>
+                 <button onClick={handlingEmail} className='bg-green-500 p-2 rounded-lg' >Send</button>
                 <br />
              <span>
                   For any of your query please send us an email on this email : webbaba@gmail.com
                   </span>
+        </div>
         </div>
     );
 };
